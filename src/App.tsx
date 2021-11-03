5d0db401e83f596408911fe80201a69b15c42a04
Lucas Eschechola <lucas.eschechola@outlook.com>
2021-11-02 22:40:16 -0300

    Add toast and auth to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import './App.css';
 import Routes from './routes/routes';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
     <div className="app">
       <Routes />
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover/>
     </div>
   );
 }
